8e1e4d808deedc9ed179d6998744ef965568ae3a
Babina, Nandor <nandor.babina@sap.com>
2018-06-25 15:51:04 +0200

    static view, change buttons to icons

--- source/forms/antragsUebersicht/StudiengangsleiterView.php
+++ source/forms/antragsUebersicht/StudiengangsleiterView.php
@@ -33,90 +33,34 @@ class StudiengangsleiterView extends moodleform {
         // 1. Angaben zum Unternehmen
         $mform->addElement('header', 'angabenZumAntragstellerAlsUnternehmen', get_string('subtitle1', 'mod_apsechs'));
         
-        $mform->addElement('text', 'vorname', get_string('vorname', 'mod_apsechs'));
-        $mform->setDefault('vorname', $dbConnectivity->getMetaData()["angesteller"]["vorname"]);
-        $mform->setType('vorname', PARAM_TEXT);
-        
-        $mform->addElement('text', 'nachname', get_string('nachname', 'mod_apsechs'));
-        $mform->setDefault('nachname', $dbConnectivity->getMetaData()["angesteller"]["nachname"]);
-        $mform->setType('nachname', PARAM_TEXT);
-        
-        $mform->addElement('text', 'email', get_string('email', 'mod_apsechs'));
-        $mform->setDefault('email', $dbConnectivity->getMetaData()["angesteller"]["email"]);
-        $mform->setType('email', PARAM_TEXT);
-        
-        $mform->addElement('text', 'tel', get_string('tel', 'mod_apsechs'));
-        $mform->setDefault('tel', $dbConnectivity->getMetaData()["angesteller"]["tel"]);
-        $mform->setType('tel', PARAM_TEXT);
-        
-        $mform->addElement('text', 'fax', get_string('fax', 'mod_apsechs'));
-        $mform->setDefault('fax', $dbConnectivity->getMetaData()["angesteller"]["fax"]);
-        $mform->setType('fax', PARAM_TEXT);
-        
-        
+        $mform->addElement('static', 'vorname', get_string('vorname', 'mod_apsechs'), $dbConnectivity->getMetaData()["angesteller"]["vorname"]);
+        $mform->addElement('static', 'nachname', get_string('nachname', 'mod_apsechs'), $dbConnectivity->getMetaData()["angesteller"]["nachname"]);        
+        $mform->addElement('static', 'email', get_string('email', 'mod_apsechs'), $dbConnectivity->getMetaData()["angesteller"]["email"]);
+        $mform->addElement('static', 'tel', get_string('tel', 'mod_apsechs'), $dbConnectivity->getMetaData()["angesteller"]["tel"]);
+        $mform->addElement('static', 'fax', get_string('fax', 'mod_apsechs'), $dbConnectivity->getMetaData()["angesteller"]["fax"]);        
         
         // 2. Angaben zum Unternehmen
         $mform->addElement('header', 'angabenZumUnternehmen', get_string('subtitle2', 'mod_apsechs'));
         $mform->closeHeaderBefore('angabenZumUnternehmen');
         
-        $mform->addElement('text', 'name', get_string('name', 'mod_apsechs'));
-        $mform->setDefault('name', $dbConnectivity->getMetaData()["unternehmen"]["name"]);
-        $mform->setType('name', PARAM_TEXT);
-        
-        $mform->addElement('text', 'branche', get_string('branche', 'mod_apsechs'));
-        $mform->setDefault('branche', $dbConnectivity->getMetaData()["unternehmen"]["branche"]);
-        $mform->setType('branche', PARAM_TEXT);
-        
-        $mform->addElement('text', 'mitgliedKammer', get_string('mitgliedKammer', 'mod_apsechs'));
-        $mform->setDefault('mitgliedKammer', $dbConnectivity->getMetaData()["unternehmen"]["mitgliedKammer"]);
-        $mform->setType('mitgliedKammer', PARAM_TEXT);
-        
-        $mform->addElement('text', 'in', get_string('in', 'mod_apsechs'));
-        $mform->setDefault('in', $dbConnectivity->getMetaData()["unternehmen"]["in"]);
-        $mform->setType('in', PARAM_TEXT);
-        
-        $mform->addElement('text', 'stadt', get_string('stadt', 'mod_apsechs'));
-        $mform->setDefault('stadt', $dbConnectivity->getMetaData()["unternehmen"]["stadt"]);
-        $mform->setType('stadt', PARAM_TEXT);
-        
-        $mform->addElement('text', 'postleitzahl', get_string('postleitzahl', 'mod_apsechs'));
-        $mform->setDefault('postleitzahl', $dbConnectivity->getMetaData()["unternehmen"]["postleitzahl"]);
-        $mform->setType('postleitzahl', PARAM_TEXT);
-        
-        $mform->addElement('text', 'anzahlMitarbeiter', get_string('anzahlMitarbeiter', 'mod_apsechs'));
-        $mform->setDefault('anzahlMitarbeiter', $dbConnectivity->getMetaData()["unternehmen"]["anzahlMitarbeiter"]);
-        $mform->setType('anzahlMitarbeiter', PARAM_TEXT);
-        
-        $mform->addElement('text', 'anzahlKaufmAusbildenden', get_string('anzahlKaufmAusb', 'mod_apsechs'));
-        $mform->setDefault('anzahlKaufmAusbildenden', $dbConnectivity->getMetaData()["unternehmen"]["anzahlKaufmAusbildenden"]);
-        $mform->setType('anzahlKaufmAusbildenden', PARAM_TEXT);
-        
-        $mform->addElement('text', 'strasse', get_string('strasse', 'mod_apsechs'));
-        $mform->setDefault('strasse', $dbConnectivity->getMetaData()["unternehmen"]["strasse"]);
-        $mform->setType('strasse', PARAM_TEXT);
-        
-        $mform->addElement('text', 'nummer', get_string('nummer', 'mod_apsechs'));
-        $mform->setDefault('nummer', $dbConnectivity->getMetaData()["unternehmen"]["nummer"]);
-        $mform->setType('nummer', PARAM_TEXT);
-        
-        $mform->addElement('text', 'anzahlTechnischenAusbildenden', get_string('anzahlTechnischenAusb', 'mod_apsechs'));
-        $mform->setDefault('anzahlTechnischenAusbildenden', $dbConnectivity->getMetaData()["unternehmen"]["anzahlTechnischenAusbildenden"]);
-        $mform->setType('anzahlTechnischenAusbildenden', PARAM_TEXT);
-        
-        $mform->addElement('text', 'anzahlSonstigerAusbildenden', get_string('anzahlSonstigerAusb', 'mod_apsechs'));
-        $mform->setDefault('anzahlSonstigerAusbildenden', $dbConnectivity->getMetaData()["unternehmen"]["anzahlSonstigerAusbildenden"]);
-        $mform->setType('anzahlSonstigerAusbildenden', PARAM_TEXT);
-        
-        
+        $mform->addElement('static', 'name', get_string('name', 'mod_apsechs'), $dbConnectivity->getMetaData()["unternehmen"]["name"]);
+        $mform->addElement('static', 'branche', get_string('branche', 'mod_apsechs'), $dbConnectivity->getMetaData()["unternehmen"]["branche"]);        
+        $mform->addElement('static', 'mitgliedKammer', get_string('mitgliedKammer', 'mod_apsechs'), $dbConnectivity->getMetaData()["unternehmen"]["mitgliedKammer"]);        
+        $mform->addElement('static', 'in', get_string('in', 'mod_apsechs'), $dbConnectivity->getMetaData()["unternehmen"]["in"]);        
+        $mform->addElement('static', 'stadt', get_string('stadt', 'mod_apsechs'), $dbConnectivity->getMetaData()["unternehmen"]["stadt"]);        
+        $mform->addElement('static', 'postleitzahl', get_string('postleitzahl', 'mod_apsechs'), $dbConnectivity->getMetaData()["unternehmen"]["postleitzahl"]);        
+        $mform->addElement('static', 'anzahlMitarbeiter', get_string('anzahlMitarbeiter', 'mod_apsechs'), $dbConnectivity->getMetaData()["unternehmen"]["anzahlMitarbeiter"]);        
+        $mform->addElement('static', 'anzahlKaufmAusbildenden', get_string('anzahlKaufmAusb', 'mod_apsechs'), $dbConnectivity->getMetaData()["unternehmen"]["anzahlKaufmAusbildenden"]);        
+        $mform->addElement('static', 'strasse', get_string('strasse', 'mod_apsechs'), $dbConnectivity->getMetaData()["unternehmen"]["strasse"]);        
+        $mform->addElement('static', 'nummer', get_string('nummer', 'mod_apsechs'), $dbConnectivity->getMetaData()["unternehmen"]["nummer"]);        
+        $mform->addElement('static', 'anzahlTechnischenAusbildenden', get_string('anzahlTechnischenAusb', 'mod_apsechs'), $dbConnectivity->getMetaData()["unternehmen"]["anzahlTechnischenAusbildenden"]);        
+        $mform->addElement('static', 'anzahlSonstigerAusbildenden', get_string('anzahlSonstigerAusb', 'mod_apsechs'), $dbConnectivity->getMetaData()["unternehmen"]["anzahlSonstigerAusbildenden"]);        
         
         // 3. Angaben zur Ausbildung
-        // TODO: set defaults
         $mform->addElement('header', 'angabenZurAusbildung', get_string('subtitle3', 'mod_apsechs'));
         $mform->closeHeaderBefore('angabenZurAusbildung');
         
-        $mform->addElement('text', 'verguetung', get_string('verguetung', 'mod_apsechs'));
-        $mform->setDefault('verguetung', $dbConnectivity->getMetaData()["ausbildung"]["verguetung"]);
-        $mform->setType('verguetung', PARAM_TEXT);
+        $mform->addElement('static', 'verguetung', get_string('verguetung', 'mod_apsechs'), $dbConnectivity->getMetaData()["ausbildung"]["verguetung"]);
         
         // radio buttons for inhalte des a.bildungplanes
         $inhalte=array();
@@ -126,27 +70,30 @@ class StudiengangsleiterView extends moodleform {
         
         // Download option
         $downloadOption = array();
-        $downloadOption[] =& $mform->createElement('static', 'lblForDownload', get_string('ausbildungsplanStud', 'mod_apsechs'));
-        $downloadOption[] =& $mform->createElement('button', 'downloadBtn', get_string('download', 'mod_apsechs'));
-        $mform->addGroup($downloadOption, 'downloadGroup', '', array(' '), false);
-        
+        $downloadOption[] =& $mform->createElement('html', '<span class="col-form-label d-inline-block">'. get_string('ausbildungsplanStud', 'mod_apsechs') .'</span>');
+        $downloadOption[] =& $mform->createElement('html', '<button style="background: url(icons/downloadIcon.png); background-repeat: no-repeat; background-size: 100%; border: none; height: 33px; width: 33px;"/>');
+        $mform->addGroup($downloadOption, 'downloadGroup', '', array(' '), false);        
         
         // 4. Angaben zur Ausbildung
         $mform->addElement('header', 'antragsbearbeitung', '4. Antragsbearbeitung');
         $mform->closeHeaderBefore('antragsbearbeitung');
         
         // radio buttons for inhalte des a.bildungplanes
-        $inhalte=array();
-        $inhalte[] = $mform->createElement('radio', 'aufnahme', '', get_string('aufnahme1', 'mod_apsechs'), 0);
-        $inhalte[] = $mform->createElement('radio', 'aufnahme', '', get_string('aufnahme2', 'mod_apsechs'), 1);
-        $inhalte[] = $mform->createElement('radio', 'aufnahme', '', get_string('aufnahme3', 'mod_apsechs'), 2);
-        $mform->addGroup($inhalte, 'aufnahmeRadio', get_string('aufnahmeText', 'mod_apsechs'), array(' '), false);
+        $mform->addElement('radio', 'aufnahme', '', get_string('aufnahme1', 'mod_apsechs'), 0);
+        // fehlt noch: date selector:
+        $inhaltOption2=array();
+        $inhaltOption2[] = $mform->createElement('radio', 'aufnahme', '', get_string('aufnahme2', 'mod_apsechs'), 1);
+        $inhaltOption2[] = $mform->createElement('date_selector', 'inhaltOpt2DS', '');
+        $mform->addGroup($inhaltOption2, 'aufnahmeRadio', get_string('aufnahmeText', 'mod_apsechs'), array(' '), false);
+        
+        $mform->addElement('radio', 'aufnahme', '', get_string('aufnahme3', 'mod_apsechs'), 2);
+        
         
         $mform->addElement('select', 'zulassung', get_string('zulassungStudiengang', 'mod_apsechs'),
                            ["nicht zutreffend"],
                            [0]);
         
-        
+        $mform->disabledIf('zulassung', 1, 'eq', 0);
         
         // Zulassungsprozess
         $mform->addElement('header', 'zulassungsprozess', get_string('subtitleFinal', 'mod_apsechs'));
@@ -164,10 +111,9 @@ class StudiengangsleiterView extends moodleform {
                 $process[] = $mform->createElement('static', 'prozessDiv' . $i, ' > ');
             }
         }
-        $mform->addGroup($currentStep, 'proz', '', array(' '), false);
+        $mform->addGroup($process, 'proz', '', array(' '), false);
         
         $mform->addElement('checkbox', 'besichtigt',  get_string('besichtigt', 'mod_apsechs'));
-        
         $mform->addElement('date_selector', 'datumUNehmenBes', get_string('datumUNBes', 'mod_apsechs'));
 
         
@@ -176,7 +122,7 @@ class StudiengangsleiterView extends moodleform {
         $mainButons=array();
         $mainButons[] =& $mform->createElement('submit', 'genehmigen', get_string('genehmigen', 'mod_apsechs'));
         $mainButons[] =& $mform->createElement('submit', 'ablehen', get_string('ablehen', 'mod_apsechs'));
-        $mainButons[] =& $mform->createElement('submit', 'drucken', get_string('drucken', 'mod_apsechs'));
+        $mainButons[] =& $mform->createElement('html', '<div class="form-group fitem"><button onclick="window.print()" style="background: url(icons/printIcon.png); background-repeat: no-repeat; background-size: 100%; border: none; height: 33px; width: 33px;"/></div>');
         $mform->addGroup($mainButons, 'mainBtns', '', array(' '), false);
         
         
