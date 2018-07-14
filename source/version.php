a695bf58a2e676eb5b047e21318fea5c4adc0297
Simon <simonwohlfahrt@live.de>
2018-07-14 11:20:16 +0200

    updated version.php

--- source/version.php
+++ source/version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_firmenzulassung'; 
-$plugin->version = 2018070700;
-$plugin->release = '2018070700';
+$plugin->version = 2018071300;
+$plugin->release = '2018071300';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->cron = 0;
